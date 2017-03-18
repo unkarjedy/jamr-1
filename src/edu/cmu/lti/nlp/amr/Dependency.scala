b302a30a62242ebbb1363c980bf4c1b457cd5cd0
unkarjedy <unkarjedy@mail.ru>
2017-03-18 18:13:07 +0300

    Reformated Dependency.scala

--- src/edu/cmu/lti/nlp/amr/Dependency.scala
+++ src/edu/cmu/lti/nlp/amr/Dependency.scala
@@ -8,14 +8,17 @@ import scala.collection.mutable.ArrayBuffer
 case class Dependency(head: Int, dependent: Int, relation: String)
 
 object Dependency {
-    def fromConll(string: String) : Dependency = {
-        val fields = string.split("\t")
-        return Dependency(fields(6).toInt-1, fields(0).toInt-1, fields(7))
-    }
-    val Stanford = """([^(]+[^-]+([0-9]+), *[^-]+([0-9]+)) *""".r
-    def fromStanford(string: String) : Dependency = {
-        val Stanford(relation, head, dependent) = string
-        return Dependency(head.toInt-1, dependent.toInt-1, relation)
-    }
+
+  private val Stanford = """([^(]+[^-]+([0-9]+), *[^-]+([0-9]+)) *""".r
+
+  def fromConll(string: String): Dependency = {
+    val fields = string.split("\t")
+    Dependency(fields(6).toInt - 1, fields(0).toInt - 1, fields(7))
+  }
+
+  def fromStanford(string: String): Dependency = {
+    val Stanford(relation, head, dependent) = string
+    Dependency(head.toInt - 1, dependent.toInt - 1, relation)
+  }
 }
 
