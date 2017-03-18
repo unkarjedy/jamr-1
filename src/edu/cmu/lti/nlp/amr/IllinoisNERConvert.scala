19e6f06837342de5215b2d92b2eebed07ac89dca
unkarjedy <unkarjedy@mail.ru>
2017-03-18 17:18:29 +0300

    Removed IllinoisNERConvert.sh left scala version

--- src/edu/cmu/lti/nlp/amr/IllinoisNERConvert.scala
+++ src/edu/cmu/lti/nlp/amr/IllinoisNERConvert.scala
@@ -21,11 +21,10 @@ case class IllinoisNERConvert(in: InputStream,
   }
 
   def convertLine(line: String): Unit = {
-    var tag = "O"
+    var tag: String = "O"
     var begin = false
-
-    // more than one space, or an empty line, is skipped
-    for (word <- line.split(" ") if word.trim != "") {
+    for (word <- line.split(" ") if word != "") {
+      // more than one space, or an empty line, is skipped
       if (word.matches("""\[[A-Z]+""")) {
         if (tag != "O") {
           err.println(" *** WARNING *** : Nested tags in Illinois NER output.\nLine = " + line + "\nSuggestion: you may want to remove any [ or ] from the input.")
@@ -34,22 +33,26 @@ case class IllinoisNERConvert(in: InputStream,
         begin = true
       } else if (!word.endsWith("]") || word.length == 1) {
         if (tag != "O") {
-          out.println(s"$word\t${if (begin) "B-" else "I-"}$tag")
+          out.println(word + "\t" + label(begin) + tag)
         } else {
-          out.println(s"$word\t$tag")
+          out.println(word + "\t" + tag)
         }
         begin = false
       } else {
         if (tag == "O") {
           err.println(" *** WARNING *** : Closing bracket outside of tag in Illinois NER output.\nLine = " + line + "\nSuggestion: you may want to remove any [ or ] from the input.")
-          out.println(s"${word.slice(0, word.length - 1)}\tO")
+          out.println(word.slice(0, word.length - 1) + "\tO")
         } else {
-          out.println(s"${word.slice(0, word.length - 1)}\t${if (begin) "B-" else "I-"}$tag")
+          out.println(word.slice(0, word.length - 1) + "\t" + label(begin) + tag)
         }
         tag = "O"
       }
     }
     out.print("\n")
+  }
+  private def label(begin: Boolean) = {
+    if (begin) "B-"
+    else "I-"
   }
 }
 
