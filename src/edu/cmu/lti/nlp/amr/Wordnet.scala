656787c35c9289ead6a10ea0398c3904aa2b8b49
Dmitrii Naumenko <Dmitrii_Naumenko@epam.com>
2017-04-27 16:47:05 +0300

    Added WordNet cache

--- src/edu/cmu/lti/nlp/amr/Wordnet.scala
+++ src/edu/cmu/lti/nlp/amr/Wordnet.scala
@@ -2,7 +2,6 @@ package edu.cmu.lti.nlp.amr
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
-
 import java.io.File
 import java.net.URL
 
@@ -11,15 +10,17 @@ import edu.mit.jwi.data.ILoadPolicy
 import edu.mit.jwi.item.POS
 import edu.mit.jwi.morph.WordnetStemmer
 
+import scala.collection.mutable
+
 object Wordnet {
   private var wordnetStemmer = Option(System.getenv("WNHOME")).map(newWordNetStemmer).orNull
 
-  def prepareWordNetStemmer(wnHome: String): Unit = {
-    wordnetStemmer = newWordNetStemmer(wnHome)
+  def prepareWordNetStemmer(wordnetHome: String): Unit = {
+    wordnetStemmer = newWordNetStemmer(wordnetHome)
   }
 
-  private def newWordNetStemmer(wnHome: String): WordnetStemmer = {
-    val path = wnHome + File.separator + "dict"
+  private def newWordNetStemmer(wordnetHomePath: String): WordnetStemmer = {
+    val path = wordnetHomePath + File.separator + "dict"
     val url = new URL("file", null, path)
     val dict = new RAMDictionary(url, ILoadPolicy.NO_LOAD)
     dict.open
@@ -27,27 +28,33 @@ object Wordnet {
   }
 
 
-  def stemmer(word: String): List[String] = {
-    var stems = List[String]()
-    for (pos <- POS.values) {
-      try {
-        stems ++= wordnetStemmer.findStems(word, pos)
-      }
-      catch {
-        case e: Throwable => Unit
-      }
-    }
+  private val wordCache = mutable.Map[String, List[String]]()
+  private val wordPosCache = mutable.Map[(String, POS), List[String]]()
 
-    stems.distinct.sorted
+  def stemmer(word: String): List[String] = {
+    val cached = wordCache.get(word)
+
+    cached.getOrElse({
+      var stems = POS.values.toList.flatMap(pos => stemmer(word, pos))
+      stems = stems.distinct.sorted
+      wordCache(word) = stems
+      stems
+    })
   }
 
   def stemmer(word: String, pos: POS): List[String] = {
-    try {
-      wordnetStemmer.findStems(word, pos).asScala.toList
-    }
-    catch {
-      case e: Throwable => List()
-    }
+    val cached = wordPosCache.get((word, pos))
+
+    cached.getOrElse({
+      try {
+        val stems = wordnetStemmer.findStems(word, pos).asScala.toList
+        wordPosCache((word, pos)) = stems
+        stems
+      }
+      catch {
+        case e: Throwable => List()
+      }
+    })
   }
 
 }
